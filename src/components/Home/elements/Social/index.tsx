aefc670bac244ca5448cb51bba2d6dddd54f3a48
free1f <liseth.yggdrasil@gmail.com>
2021-05-22 19:43:56 -0400

    BB-16

---
+++ src/components/Home/elements/Social/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import { Button } from '@components'
+
+const Social = () => {
+
+  return (
+    <section className={styles._content}>
+        <div className={styles._socialImageContainer}>
+            <div className={styles._imageContainer}></div>
+        </div>
+        <div className={styles._socialInfoContainer}>
+            <div className={styles._socialTitleContainer}>
+                <h2 className={styles._socialTitle}>@broccolibrand</h2>
+            </div>
+            <div className={styles._socialDescriptionContainer}>
+                <p className={styles._socialDescription}>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit id, rutrum ac vitae sed varius potenti sociosqu, 
+                    lobortis cum pharetra orci et platea augue. Accumsan aenean ornare interdum luctus sociosqu condimentum pellentesque, 
+                    urna feugiat tempus placerat hendrerit donec ut, tellus leo aptent tortor euismod conubia. 
+                    Ullamcorper condimentum laoreet facilisis malesuada nunc a hac, potenti proin per aptent venenatis semper, 
+                    facilisi est molestie justo euismod porttitor.
+                </p>
+            </div>
+            <div className={styles._buttonContainer}>
+                <Button text="FOLLOW US" colorText="black" borderColor="black" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Social
