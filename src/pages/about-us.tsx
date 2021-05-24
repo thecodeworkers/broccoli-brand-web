7ec9e8bdf469e39df67366564b99ca684e59827f
free1f <liseth.yggdrasil@gmail.com>
2021-05-24 07:08:14 -0400

    about

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { wrapper } from '@store'
+/* import { getPages } from '@store/actions' */
+import { AboutUs } from '@components'
+
+const AboutUsPage = () => {
+
+ /*  const { page: { aboutPage: { about } } } = useSelector((state: any) => state) */
+
+  return (
+    <AboutUs />
+  )
+}
+
+/* export const getServerSideProps = wrapper.getServerSideProps(
+  ({ store }) => store.dispatch(getPages('aboutPage'))
+)
+ */
+export default AboutUsPage
