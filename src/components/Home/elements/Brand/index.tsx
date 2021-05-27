b165d105cc37ad9de999f1b0f258aeea1f3b1d25
free1f <liseth.yggdrasil@gmail.com>
2021-05-27 09:55:38 -0400

    animation almost done

--- src/components/Home/elements/Brand/index.tsx
+++ src/components/Home/elements/Brand/index.tsx
@@ -4,19 +4,17 @@ import styles from './styles.module.scss'
 const Brand = () => {
 
 	const [pos, setPos] = useState(0)
+	const [wide, setWide] = useState(0)
 
 	useEffect(() => {
 		console.log("in useeffect")
 		window.addEventListener("scroll", (event) => {
 			let scroll = window.scrollY
-			// console.log(scroll);
-			setPos(scroll)
+			
 			if(scroll > 700) {
-				let element = document.getElementById("element")
+				// let element = document.getElementById("element")
 				// element.classList.add("animateMe");
 				setPos(scroll)
-				console.log(scroll);
-				console.log("here")
 			}
 		});
 	})
@@ -24,17 +22,14 @@ const Brand = () => {
 	return (
 		<section className={styles._brandContent}>
 			<div className={styles._horizontalScroll}>
-				{/* <div className={[styles._firstBanner, styles._horizontalScroll1].join(" ")}>1</div> */}
-				{/* <div className={styles._secondBanner}>2</div> */}
-				<img id="element" style={{transform: `translate(-${pos}px`}} src="images/backgrounds/Banner_animado1.png" alt="" />
-				<img src="images/backgrounds/Banner_animado2.png" alt="" />
+				<img id="element" style={{transform: `translate(-${pos}px)`}} src="images/backgrounds/Banner_animado1.png" alt="" />
+				<img style={{transform: `translate(calc(-3000px + ${pos}px))`}} src="images/backgrounds/Banner_animado2.png" alt="" />
 			</div>
 			<style jsx>
 			{`
 			animateMe: {
 				transform: translateX(${pos});
 			}
-			
 			`}
 			</style>
 		</section>
