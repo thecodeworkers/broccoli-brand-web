c22bc3b2012164a05ebe98b3bc7cf8ae6d72dc8e
free1f <liseth.yggdrasil@gmail.com>
2021-05-27 19:51:13 -0400

    background images

--- src/components/Home/elements/Brand/index.tsx
+++ src/components/Home/elements/Brand/index.tsx
@@ -4,7 +4,6 @@ import styles from './styles.module.scss'
 const Brand = () => {
 
 	const [pos, setPos] = useState(0)
-	const [wide, setWide] = useState(0)
 
 	useEffect(() => {
 		console.log("in useeffect")
@@ -12,27 +11,32 @@ const Brand = () => {
 			let scroll = window.scrollY
 			
 			if(scroll > 700) {
-				// let element = document.getElementById("element")
-				// element.classList.add("animateMe");
 				setPos(scroll)
 			}
 		});
 	})
 
 	return (
-		<section className={styles._brandContent}>
-			<div className={styles._horizontalScroll}>
-				<div className={styles._imageContainer}>
-					<img id="element" style={{transform: `translate(-${pos}px)`}} src="images/backgrounds/Banner_animado1.png" alt="" />
+		<section className={styles._principalBrandContent}>
+				<div className={styles._imageContainer} style={{transform: `translate(-${pos}px)`}}>
+					{/* <img id="element" style={{transform: `translate(-${pos}px)`}} src="images/backgrounds/Banner_animado1.png" alt="" /> */}
+					<div className='_firstBrand' ></div>
 				</div>
-				<div className={styles._imageContainer}>
-					<img style={{transform: `translate(calc(-3000px + ${pos}px))`}} src="images/backgrounds/Banner_animado2.png" alt="" />
+				<div className={styles._imageContainer} style={{transform: `translate(calc(-3000px + ${pos}px))`}}>
+					{/* <img style={{transform: `translate(calc(-3000px + ${pos}px))`}} src="images/backgrounds/Banner_animado2.png" alt="" /> */}
+					<div className='_secondBrand' ></div>
 				</div>
-			</div>
 			<style jsx>
 			{`
-			animateMe: {
-				transform: translateX(${pos});
+			._firstBrand {
+				background-image: url('images/backgrounds/Banner_animado1.png');
+				background-repeat: repeat-x;
+				height: inherit;
+			}
+			._secondBrand {
+				background-image: url('images/backgrounds/Banner_animado2.png');
+				background-repeat: repeat-x;
+				height: inherit;
 			}
 			`}
 			</style>
