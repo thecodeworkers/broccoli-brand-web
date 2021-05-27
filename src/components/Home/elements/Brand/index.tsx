8afc4000280b5fad020503c7ca4317580773bc7d
free1f <liseth.yggdrasil@gmail.com>
2021-05-27 08:10:01 -0400

    first animation

--- src/components/Home/elements/Brand/index.tsx
+++ src/components/Home/elements/Brand/index.tsx
@@ -3,39 +3,42 @@ import styles from './styles.module.scss'
 
 const Brand = () => {
 
-    const [pos, setPos] = useState(0)
+	const [pos, setPos] = useState(0)
 
-    useEffect(() => {
-        window.addEventListener("scroll", (event) => {
-            let scroll = window.scrollY
+	useEffect(() => {
+		console.log("in useeffect")
+		window.addEventListener("scroll", (event) => {
+			let scroll = window.scrollY
+			// console.log(scroll);
+			setPos(scroll)
+			if(scroll > 700) {
+				let element = document.getElementById("element")
+				// element.classList.add("animateMe");
+				setPos(scroll)
+				console.log(scroll);
+				console.log("here")
+			}
+		});
+	})
 
-            if(scroll > 700) {
-                let element = document.getElementById("element")
-                element.classList.add("animateMe");
-                setPos(scroll)
-                console.log("here")
-            }
-        });
-    })
-
-    return (
-        <section className={styles._brandContent}>
-            <div className={styles._horizontalScroll}>
-                {/* <div className={[styles._firstBanner, styles._horizontalScroll1].join(" ")}>1</div> */}
-                {/* <div className={styles._secondBanner}>2</div> */}
-                <img id="element" src="images/backgrounds/Banner_animado1.png" alt="" />
-                <img src="images/backgrounds/Banner_animado2.png" alt="" />
-            </div>
-            <style jsx>
-            {`
-            animateMe: {
-                transform: translateX(${pos});
-            }
-            
-            `}
-            </style>
-        </section>
-    )
+	return (
+		<section className={styles._brandContent}>
+			<div className={styles._horizontalScroll}>
+				{/* <div className={[styles._firstBanner, styles._horizontalScroll1].join(" ")}>1</div> */}
+				{/* <div className={styles._secondBanner}>2</div> */}
+				<img id="element" style={{transform: `translate(-${pos}px`}} src="images/backgrounds/Banner_animado1.png" alt="" />
+				<img src="images/backgrounds/Banner_animado2.png" alt="" />
+			</div>
+			<style jsx>
+			{`
+			animateMe: {
+				transform: translateX(${pos});
+			}
+			
+			`}
+			</style>
+		</section>
+	)
 }
 
 export default Brand
