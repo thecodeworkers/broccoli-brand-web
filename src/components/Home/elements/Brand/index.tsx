8ff3e1878ceb2f018a9399c20c3b0cc03a5c8c90
free1f <liseth.yggdrasil@gmail.com>
2021-05-27 20:28:45 -0400

    resize

--- src/components/Home/elements/Brand/index.tsx
+++ src/components/Home/elements/Brand/index.tsx
@@ -6,23 +6,19 @@ const Brand = () => {
 	const [pos, setPos] = useState(0)
 
 	useEffect(() => {
-		console.log("in useeffect")
-		window.addEventListener("scroll", (event) => {
+		window.addEventListener("scroll", () => {
 			let scroll = window.scrollY
-			
-			if(scroll > 700) {
-				setPos(scroll)
-			}
+			if(scroll > 700) setPos(scroll)
 		});
 	})
 
 	return (
 		<section className={styles._principalBrandContent}>
-				<div className={styles._imageContainer} style={{transform: `translate(-${pos}px)`}}>
+				<div className={styles._imageContainer} style={{transform: `translate(calc(-${pos}px + 300px))`}}>
 					{/* <img id="element" style={{transform: `translate(-${pos}px)`}} src="images/backgrounds/Banner_animado1.png" alt="" /> */}
 					<div className='_firstBrand' ></div>
 				</div>
-				<div className={styles._imageContainer} style={{transform: `translate(calc(-3000px + ${pos}px))`}}>
+				<div className={styles._imageContainer} style={{transform: `translate(calc(${pos}px - 7500px))`}}>
 					{/* <img style={{transform: `translate(calc(-3000px + ${pos}px))`}} src="images/backgrounds/Banner_animado2.png" alt="" /> */}
 					<div className='_secondBrand' ></div>
 				</div>
@@ -31,12 +27,14 @@ const Brand = () => {
 			._firstBrand {
 				background-image: url('images/backgrounds/Banner_animado1.png');
 				background-repeat: repeat-x;
-				height: inherit;
+				background-size: contain;
+				height: 27.5vh;
 			}
 			._secondBrand {
 				background-image: url('images/backgrounds/Banner_animado2.png');
 				background-repeat: repeat-x;
-				height: inherit;
+				background-size: contain;
+				height: 27.5vh;
 			}
 			`}
 			</style>
