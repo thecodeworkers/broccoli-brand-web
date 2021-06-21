892c75c936aeb229b1101e4aaf6970f3fa5f6e63
free1f <liseth.yggdrasil@gmail.com>
2021-06-21 04:18:33 -0400

    first collection integration

--- src/components/ResponsiveShop/index.tsx
+++ src/components/ResponsiveShop/index.tsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './styles.module.scss'
 import { Bag } from '@images/svg'
+import { useDispatch } from 'react-redux'
+import { addToCar } from '@store/actions'
 
-const ResponsiveShop = () => {
+const ResponsiveShop = ({ data = null }) => {
+
+  const dispatch = useDispatch()
+  const add = () => { if (data) dispatch(addToCar(data?.databaseId, 1)) }
   
   return (
     <>
       <div className={['_productImage', styles._productImage].join(" ")}>
-        <div className={styles._bag}>
+        <div className={styles._bag} onClick={add}>
           <Bag width='20' height='20' fill='white' />
         </div>
       </div>
       <style jsx>{`
         ._productImage {
-          background-image: url(https://picsum.photos/700/700);
+          background-image: url(${data?.image?.mediaItemUrl});
           background-repeat: no-repeat;
           background-position: center;
-          background-size: 100% 100%;
+          background-size: cover;
         }
       `}</style>  
     </>
